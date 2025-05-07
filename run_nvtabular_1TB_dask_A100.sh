b34eeb85f1960aedc715c9e186fa240eed14d3eb
Arith2 <1787753693@qq.com>
2025-05-07 21:17:34 +0200

    nvtabular Criteo 1TB

--- run_nvtabular_1TB_dask_A100.sh
+++ run_nvtabular_1TB_dask_A100.sh
@@ -60,8 +60,8 @@ for part_size in "${PART_SIZES[@]}"; do
         clear_gpu_memory
         
         # Start GPU monitoring in background
-        monitor_gpu > "logs/gpu_util_${part_size}_run${run}.log" &
-        MONITOR_PID=$!
+        # monitor_gpu > "logs/gpu_util_${part_size}_run${run}.log" &
+        # MONITOR_PID=$!
         
         # Run the preprocessing script
         echo "Running preprocessing with part_size=$part_size"
@@ -71,8 +71,8 @@ for part_size in "${PART_SIZES[@]}"; do
             --part_size "$part_size" \
             2>&1 | tee "logs/output_${part_size}_run${run}.log"
         
-        # Stop GPU monitoring
-        kill $MONITOR_PID
+        # # Stop GPU monitoring
+        # kill $MONITOR_PID
         
         # Clear GPU memory after each run
         clear_gpu_memory
